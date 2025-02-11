8cefb5b905904f629ae0b9e18f5080ab4ddaeb71
SwannMartineau <swann.martineau@gmail.com>
2025-02-11 15:33:13 +0100

    feat add header

--- header/src/button.tsx
+++ header/src/button.tsx
@@ -1,3 +1,10 @@
 export default function Button() {
-    return <div>Provider hehe button</div>;
+    return <header className="bg-white shadow-md p-4 flex justify-between items-center">
+    <div className="text-xl font-bold">Mon Site</div>
+    <nav className="hidden md:flex space-x-4">
+      <a href="#" className="text-gray-700 hover:text-black">Accueil</a>
+      <a href="#" className="text-gray-700 hover:text-black">Services</a>
+      <a href="#" className="text-gray-700 hover:text-black">Contact</a>
+    </nav>
+  </header>;
   }
--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -5,11 +5,11 @@ import Header from 'header/button';
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
       <div>
         <Header />
       </div>
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
 };
