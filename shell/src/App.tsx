0ba49656fc353877ff0ff25599708c00d026c907
SwannMartineau <swann.martineau@gmail.com>
2025-02-11 15:22:21 +0100

    feat: add provider and consumer microfront

--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
+// The remote component provided by federation_provider
+import Header from 'header/button';
 
 const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <div>
+        <Header />
+      </div>
     </div>
   );
 };
